5606f1f22debef2f386eb1a52eee7c26ecd24d1e
Joelcalanche <joelcalanche96@gmail.com>
2022-01-04 10:46:44 +0000

    bugg solve!

--- nlp_app_3.py
+++ nlp_app_3.py
@@ -83,8 +83,8 @@ print("este es mi experimento",len(df_tmp))
 print(len(r_1))
 unique_titles = r_1["title"].unique().tolist()
 selected_title = st.sidebar.selectbox('titles', unique_titles)
-print(selected_title)
-print(len(selected_title))
+# print(selected_title)
+# print(len(selected_title))
 
 
 # segundo filtro
@@ -95,117 +95,129 @@ r_2_str = r_2.tolist()
 # st.header('Display Player Stats of Selected Team(s)')
 # st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
 # # print(df_selected_team)
-st.write(r_2)
+# if r_2.l
+if not r_2_str:
+    print("Empty")
+    st.write("## Sorry no news found for this entry")
+else:
+    print("Not Empty")
+    st.write(r_2)
 
 
 
 # Function for Sumy Summarization
 def sumy_summarizer(docx):
-	parser = PlaintextParser.from_string(docx,Tokenizer("english"))
-	lex_summarizer = LexRankSummarizer()
-	summary = lex_summarizer(parser.document,3)
-	summary_list = [str(sentence) for sentence in summary]
-	result = ' '.join(summary_list)
-	return result
+    parser = PlaintextParser.from_string(docx,Tokenizer("english"))
+    lex_summarizer = LexRankSummarizer()
+    summary = lex_summarizer(parser.document,3)
+    summary_list = [str(sentence) for sentence in summary]
+    result = ' '.join(summary_list)
+    return result
 
 # Function to Analyse Tokens and Lemma
 @st.cache
 def text_analyzer(my_text):
-	#before 'en'------> 'en_core_web_sm'
-	nlp = spacy.load("en_core_web_sm")
-	docx = nlp(my_text)
-	# tokens = [ token.text for token in docx]
-	allData = [('"Token":{},\n"Lemma":{}'.format(token.text,token.lemma_))for token in docx ]
-	return allData
+    #before 'en'------> 'en_core_web_sm'
+    nlp = spacy.load("en_core_web_sm")
+    docx = nlp(my_text)
+    # tokens = [ token.text for token in docx]
+    allData = [('"Token":{},\n"Lemma":{}'.format(token.text,token.lemma_))for token in docx ]
+    return allData
 
 # Function For Extracting Entities
 @st.cache
 def entity_analyzer(my_text):
-	#before 'en'------> 'en_core_web_sm'
-	nlp = spacy.load("en_core_web_sm")
-	docx = nlp(my_text)
-	tokens = [ token.text for token in docx]
-	entities = [(entity.text,entity.label_)for entity in docx.ents]
-	allData = ['"Token":{},\n"Entities":{}'.format(tokens,entities)]
-	return allData
+    #before 'en'------> 'en_core_web_sm'
+    nlp = spacy.load("en_core_web_sm")
+    docx = nlp(my_text)
+    tokens = [ token.text for token in docx]
+    entities = [(entity.text,entity.label_)for entity in docx.ents]
+    allData = ['"Token":{},\n"Entities":{}'.format(tokens,entities)]
+    return allData
 
 
 def main():
-	""" NLP Based App with Streamlit """
+    """ NLP Based App with Streamlit """
 
-	# Title
-	st.title("NLPiffy with Streamlit to deeplabsearch")
-	st.subheader("Natural Language Processing On the Go..")
-	st.markdown("""
-    	#### Description
-    	+ This is a Natural Language Processing(NLP) Based App useful for basic NLP task
-    	Tokenization,NER,Sentiment,Summarization
-    	""")
+    # Title
+    st.title("NLPiffy with Streamlit to deeplabsearch")
+    st.subheader("Natural Language Processing On the Go..")
+    st.markdown("""
+        #### Description
+        + This is a Natural Language Processing(NLP) Based App useful for basic NLP task
+        Tokenization,NER,Sentiment,Summarization
+        """)
+    if not r_2_str:
+        st.write("""## wrong entry
+        
+        No record could be found for the selected year, month and day, try another combination of those suggested, check the database
+        
+        """)
+    else:
+        # Tokenization
+        if st.checkbox("Show Tokens and Lemma"):
+            st.subheader("Tokenize Your Text")
 
-	# Tokenization
-	if st.checkbox("Show Tokens and Lemma"):
-		st.subheader("Tokenize Your Text")
+            message = r_2_str
+            st.write(message[0])
+            if st.button("Analyze"):
+                nlp_result = text_analyzer(message[0])
+                st.json(nlp_result)
 
-		message = r_2_str
-		st.write(message[0])
-		if st.button("Analyze"):
-			nlp_result = text_analyzer(message[0])
-			st.json(nlp_result)
+        # Entity Extraction
+        elif st.checkbox("Show Named Entities"):
+            st.subheader("Analyze Your Text")
 
-	# Entity Extraction
-	if st.checkbox("Show Named Entities"):
-		st.subheader("Analyze Your Text")
+            message = r_2_str
+            st.write(message[0])
+            if st.button("Extract"):
+                entity_result = entity_analyzer(message[0])
+                st.json(entity_result)
 
-		message = r_2_str
-		st.write(message[0])
-		if st.button("Extract"):
-			entity_result = entity_analyzer(message[0])
-			st.json(entity_result)
+        # Sentiment Analysis
+        elif st.checkbox("Show Sentiment Analysis"):
+            st.subheader("Analyse Your Text")
 
-	# Sentiment Analysis
-	if st.checkbox("Show Sentiment Analysis"):
-		st.subheader("Analyse Your Text")
+            message = r_2_str
+            st.write(message[0])
+            if st.button("Analyze"):
+                blob = TextBlob(message[0])
+                result_sentiment = blob.sentiment
+                st.success(result_sentiment)
 
-		message = r_2_str
-		st.write(message[0])
-		if st.button("Analyze"):
-			blob = TextBlob(message[0])
-			result_sentiment = blob.sentiment
-			st.success(result_sentiment)
+        #Summarization
+        elif st.checkbox("Show Text Summarization"):
+            st.subheader("Summarize Your Text")
 
-	#Summarization
-	if st.checkbox("Show Text Summarization"):
-		st.subheader("Summarize Your Text")
+            message = r_2_str
+            st.write(message[0])
+            summary_options = st.selectbox("Choose Summarizer",['sumy'])
+            if st.button("Summarize"):
+                if summary_options == 'sumy':
+                    st.text("Using Sumy Summarizer ..")
+                    summary_result = sumy_summarizer(message[0])
+                # elif summary_options == 'gensim':
+                # 	st.text("Using Gensim Summarizer ..")
+                # 	summary_result = summarize(rawtext)
+                # else:
+                # 	st.warning("Using Default Summarizer")
+                # 	st.text("Using Gensim Summarizer ..")
+                # 	summary_result = summarize(rawtext)
 
-		message = r_2_str
-		st.write(message[0])
-		summary_options = st.selectbox("Choose Summarizer",['sumy'])
-		if st.button("Summarize"):
-			if summary_options == 'sumy':
-				st.text("Using Sumy Summarizer ..")
-				summary_result = sumy_summarizer(message[0])
-			# elif summary_options == 'gensim':
-			# 	st.text("Using Gensim Summarizer ..")
-			# 	summary_result = summarize(rawtext)
-			# else:
-			# 	st.warning("Using Default Summarizer")
-			# 	st.text("Using Gensim Summarizer ..")
-			# 	summary_result = summarize(rawtext)
-
-		
-			st.success(summary_result)
+            
+                st.success(summary_result)
 
 
 
-	st.sidebar.subheader("About App")
-	st.sidebar.text("NLPiffy App with Streamlit")
-	st.sidebar.info("Cudos to the Streamlit Team")
-	
+    st.sidebar.subheader("About App")
+    st.sidebar.text("NLPiffy App with Streamlit")
+    st.sidebar.info("Cudos to the Streamlit Team")
+    
 
-	st.sidebar.subheader("By")
-	st.sidebar.text("Joel_calanche")
-	st.sidebar.text("^_^")
-	
+    st.sidebar.subheader("By")
+    st.sidebar.text("Joel_calanche")
+    st.sidebar.text("^_^")
+    
 
 if __name__ == '__main__':
-	main()
+    main()
